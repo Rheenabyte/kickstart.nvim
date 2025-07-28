7f225dd0323ee6a94e7753477284bb23dcb294eb
Rheenabyte <rheenabyte@gmail.com>
2025-07-27 22:48:30 -0400

    Added <leader>jk and <leader>kj to vim.cmd.Ex

--- init.lua
+++ init.lua
@@ -173,6 +173,9 @@ vim.o.confirm = true
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('n', '<leader>jk', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>kj', vim.cmd.Ex)
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
